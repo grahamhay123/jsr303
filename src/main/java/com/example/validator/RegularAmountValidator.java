a2ab7d6db92f3dda53916f7aa75811a92e40e57e
Graham Hay <grahamhay@aol.com>
2020-08-03 22:45:24 +0100

    Fifth commit tidies isValid method in RegularAmountValidator class to reduce line count

--- src/main/java/com/example/validator/RegularAmountValidator.java
+++ src/main/java/com/example/validator/RegularAmountValidator.java
@@ -24,8 +24,6 @@ public class RegularAmountValidator implements
 
         boolean result = true;
 
-        String amountString = regularAmount.getAmount();
-
         // Null frequency gives no validation error
         // Weekly and Monthly amounts give no validation error
         // Non-numeric or blank amount (whitespaces only) gives no validation error
@@ -34,32 +32,39 @@ public class RegularAmountValidator implements
             regularAmount.getFrequency().multipleOfAWeek &&
             regularAmount.getFrequency() != Frequency.WEEK &&
             regularAmount.getFrequency() != Frequency.MONTH &&
-            !amountIsNonNumericOrBlank(amountString) ) {
+            !amountIsNonNumericOrBlank(regularAmount.getAmount()) ) {
             // Monetary amount must be divisible to a weekly value that is a whole number of pence
-            double regularAmt = Double.parseDouble(amountString);
-            int weeks = regularAmount.getFrequency().weeks;
-
-            // Convert to BigDecimals to perform precision division
-            BigDecimal aDec = BigDecimal.valueOf(regularAmt);
-            BigDecimal bigDecWeeks = BigDecimal.valueOf((long)weeks);
-            // Calculate weekly amount using BigDecimal values to avoid precision errors when dividing
-            // Round up to 3 decimal places to see if the value is not divisible to just 2 decimal places signifying whole pence
-            BigDecimal weeklyAmountDec = aDec.divide(bigDecWeeks, 3, RoundingMode.HALF_UP);
-            // Remove trailing zeros
-            double weeklyAmount = weeklyAmountDec.doubleValue();
-            // Find the number of decimal places in weekly amount
-            String weeklyAmountString = Double.toString(Math.abs(weeklyAmount));
-            int integerPart = weeklyAmountString.indexOf('.');
-            int numDecimalPlaces = weeklyAmountString.length() - integerPart - 1;
-
-            // Validation error if there are more than 2 decimal places after the '.'
-            if (numDecimalPlaces > 2) {
-                result = false;
-            }
-
+            result = hasWholeNumberOfPence(calculateWeeklyAmountTo3DecimalPlaces(regularAmount));
         }
 
         return result;
+    }
+
+    private boolean hasWholeNumberOfPence(double weeklyAmount) {
+        // Find the number of decimal places in weekly amount
+        String weeklyAmountString = Double.toString(Math.abs(weeklyAmount));
+        int integerPart = weeklyAmountString.indexOf('.');
+        int numDecimalPlaces = weeklyAmountString.length() - integerPart - 1;
+
+        // Validation error if there are more than 2 decimal places after the '.'
+        return numDecimalPlaces <= 2;
+
+    }
+
+    private double calculateWeeklyAmountTo3DecimalPlaces(RegularAmount regularAmount) {
+        String amountString = regularAmount.getAmount();
+        double regularAmt = Double.parseDouble(amountString);
+        int weeks = regularAmount.getFrequency().weeks;
+
+        // Convert to BigDecimals to perform precision division
+        BigDecimal aDec = BigDecimal.valueOf(regularAmt);
+        BigDecimal bigDecWeeks = BigDecimal.valueOf((long)weeks);
+        // Calculate weekly amount using BigDecimal values to avoid precision errors when dividing
+        // Round up to 3 decimal places to see if the value is not divisible to just 2 decimal places signifying whole pence
+        BigDecimal weeklyAmountDec = aDec.divide(bigDecWeeks, 3, RoundingMode.HALF_UP);
+        // Remove trailing zeros
+        return weeklyAmountDec.doubleValue();
+
     }
 
     private boolean amountIsNonNumericOrBlank(String amount) {

15a9c2e47b779935b12a2295eed251445c467322
grahamhay123 <46786939+grahamhay123@users.noreply.github.com>
2021-01-31 21:28:18 +0000

    Update RegularAmount.java
    Committed 1 to remote master

--- src/main/java/com/example/jsr303assignment/RegularAmount.java
+++ src/main/java/com/example/jsr303assignment/RegularAmount.java
@@ -7,12 +7,7 @@ public class RegularAmount {
     private Frequency frequency;
     private String amount;
     
-    private String DEVELOPER_UPDATE_LOCAL1;
-    private String DEVELOPER_UPDATE2;
-    private String DEVELOPER_UPDATE3;
-    private String DEVELOPER_UPDATE5;
-    private String DEVELOPER_UPDATE9;
-    private String DEVELOPER_UPDATE10;
+    private String DEVELOPER_UPDATE1;
 
     public Frequency getFrequency() {
         return frequency;

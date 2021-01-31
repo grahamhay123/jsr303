7e20ec3be39dcdb845e5ac4c60a1e448d333a725
Graham Hay <grahamhay@aol.com>
2021-01-31 02:24:14 +0000

    Committing DEVELOPER_UPDATE_LOCAL1

--- src/main/java/com/example/jsr303assignment/RegularAmount.java
+++ src/main/java/com/example/jsr303assignment/RegularAmount.java
@@ -7,16 +7,7 @@ public class RegularAmount {
     private Frequency frequency;
     private String amount;
     
-    private String DEVELOPER_UPDATE;
-    private String DEVELOPER_UPDATE2;
-    private String DEVELOPER_UPDATE3_LOCAL;
-    private String DEVELOPER_UPDATE4_LOCAL;
-
-    private String DEVELOPER_UPDATE5;
-    private String DEVELOPER_UPDATE6_LOCAL_MASTER;
-    private String DEVELOPER_UPDATE7;
-    private String DEVELOPER_UPDATE8_LOCAL;
-    private String DEVELOPER_UPDATE9;
+    private String DEVELOPER_UPDATE_LOCAL1;
 
     public Frequency getFrequency() {
         return frequency;

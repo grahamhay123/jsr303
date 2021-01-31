35211493b8eafc1d97560f1e1393f1fb1b989e6c
grahamhay123 <46786939+grahamhay123@users.noreply.github.com>
2021-01-31 02:03:34 +0000

    Update RegularAmount.java
    Committed DEVELOPER_UPDATE9 in REMOTE MASTER

--- src/main/java/com/example/jsr303assignment/RegularAmount.java
+++ src/main/java/com/example/jsr303assignment/RegularAmount.java
@@ -13,6 +13,7 @@ public class RegularAmount {
     private String DEVELOPER_UPDATE5;
     private String DEVELOPER_UPDATE6_LOCAL_MASTER;
     private String DEVELOPER_UPDATE7;
+    private String DEVELOPER_UPDATE9;
 
     public Frequency getFrequency() {
         return frequency;
